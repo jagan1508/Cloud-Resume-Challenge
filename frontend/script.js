841211bad4d94298422a531a21d9abbfcbdc0122
jagan1508 <jaganathbabu025@gmail.com>
2026-06-17 09:57:46 +0530

    changed backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function getVisitorCount() {
     try {
-        const response=await fetch ("https://visitorsszc--visitorsszc-main.thankfulpebble-22e3a736.westus2.azurecontainerapps.io/visitors-count");
+        const response=await fetch ("https://visitorsszc.thankfulpebble-22e3a736.westus2.azurecontainerapps.io/visitors-count");
         const data=await response.json();
         document.getElementById("visitor-count").innerText = data.visitors_count ;
     }
